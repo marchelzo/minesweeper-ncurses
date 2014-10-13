7a001833b88d9ac854603e41bcc310c40a7e52f1
Bradley Garagan <bradleygaragab@live.ca>
2014-10-12 22:40:13 -0300

    add controls and some basic game functionality

--- minesweeper.c
+++ minesweeper.c
@@ -1,8 +1,12 @@
 #include <curses.h>
 #include <stdint.h>
+#include <inttypes.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include <time.h>
+
+#define KEY_SPACE ' '
+#define KEY_RETURN '\r'
 
 #define SQ_FLAGGED_CHAR   '#'
 #define SQ_UNCLICKED_CHAR '*'
@@ -20,33 +24,53 @@ typedef enum { FLAGGED, CLICKED, UNCLICKED } SquareState;
 
 typedef enum { BEGINNER, INTERMEDIATE, EXPERT } Difficulty;
 
-typedef struct {
+typedef struct square_t {
+    struct square_t **touching;
     SquareState state;
+    uint8_t numTouching;
     uint8_t surroundingMines;
     bool hasMine;
 } Square;
 
 typedef struct {
     Square *squares;
+    int x, y;
     uint16_t boardSize;
     uint16_t numMines;
     bool gameOver;
 } Game;
 
+void setUp(void);
+
 void quitWithError(const char *msg);
 
 void drawGame(Game*);
+
+int moveLeft(Game*);
+
+int moveRight(Game*);
+
+int moveUp(Game*);
+
+int moveDown(Game*);
+
+void pollInput(Game*);
+
+void clickSquare(Game*);
+
+void flagSquare(Game*);
 
 Game *newGame(Difficulty);
 
 int main(int argc, char *argv[])
 {
-    /* seed the random number generator */
-    srand(time(NULL));
+    setUp();
     Game *g = newGame(BEGINNER);
-    initscr();
-    drawGame(g);
-    getch();
+    while (!g->gameOver) {
+        drawGame(g);
+        pollInput(g);
+    }
+    free(g);
     return 0;
 }
 
@@ -55,6 +79,8 @@ Game *newGame(Difficulty d)
     uint16_t numMines;
     uint16_t boardSize;
     Game *g = malloc(sizeof(Game));
+    g->x = 0;
+    g->y = 0;
 
     switch (d) {
     case BEGINNER:
@@ -85,6 +111,24 @@ Game *newGame(Difficulty d)
         g->squares[i].state = UNCLICKED;
         g->squares[i].surroundingMines = 0;
         g->squares[i].hasMine = false;
+        g->squares[i].touching = malloc(sizeof(Square*) * 8);
+        g->squares[i].numTouching = 0;
+    }
+
+    /* create the links between touching squares */
+    for (int i = 0; i < numSquares; ++i) {
+        uint16_t surrounding[] = {i + 1,
+                                  i - 1,
+                                  i + boardSize,
+                                  i + boardSize + 1,
+                                  i + boardSize - 1,
+                                  i - boardSize, i - boardSize + 1, i - boardSize - 1
+                                  };
+        for (int j = 0; j < 8; ++j) {
+            if (surrounding[j] >= 0 && surrounding[j] < numSquares) {
+                g->squares[i].touching[g->squares[i].numTouching++] = (g->squares + surrounding[j]);
+            }
+        }
     }
     
     int minesAdded = 0;
@@ -110,7 +154,7 @@ void drawGame(Game *g)
 {
     clear();
     for (uint16_t r = 0; r < g->boardSize; ++r) {
-        for (uint16_t c = 0; c < g->boardSize; ++r) {
+        for (uint16_t c = 0; c < g->boardSize; ++c) {
             char sqChar;
             uint16_t sqPos = g->boardSize * r + c;
             switch (g->squares[sqPos].state) {
@@ -128,10 +172,113 @@ void drawGame(Game *g)
         }
         addch('\n');
     }
+    /* debug stuff */
+    printw("\nx: %d\ny: %d\n", g->x, g->y);
+    move(g->y, g->x);
 }
 
 void quitWithError(const char *msg)
 {
     fputs(msg, stderr);
     exit(EXIT_FAILURE);
+}
+
+void setUp(void)
+{
+    srand(time(NULL));
+    initscr();
+    noecho();
+    raw();
+    cbreak();
+    keypad(stdscr, true);
+}
+
+int moveLeft(Game *g)
+{
+    if (g->x > 0) {
+        g->x -= 1;
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+int moveRight(Game *g)
+{
+    if (g->x + 1 < g->boardSize) {
+        g->x += 1;
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+int moveUp(Game *g)
+{
+    if (g->y > 0) {
+        g->y -= 1;
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+int moveDown(Game *g)
+{
+    if (g->y + 1 < (int) g->boardSize) {
+        g->y += 1;
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+void pollInput(Game *g)
+{
+    int pressed = getch();
+    switch (pressed) {
+    case KEY_UP:
+        moveUp(g);
+        break;
+    case KEY_DOWN:
+        moveDown(g);
+        break;
+    case KEY_RIGHT:
+        moveRight(g);
+        break;
+    case KEY_LEFT:
+        moveLeft(g);
+        break;
+    case KEY_RETURN:
+        clickSquare(g);
+        break;
+    case KEY_SPACE:
+        flagSquare(g);
+        break;
+    case 27:
+        quitWithError("game over");
+        break;
+    }
+    flushinp();
+}
+
+void clickSquare(Game *g)
+{
+    Square *sq = g->squares + (g->y * g->boardSize + g->x);
+    if (sq->state == UNCLICKED) {
+        if (sq->hasMine)
+            g->gameOver = true;
+        else {
+            sq->state = CLICKED;
+        }
+    }
+}
+
+void flagSquare(Game *g)
+{
+    Square *sq = g->squares + (g->y * g->boardSize + g->x);
+    if (sq->state == FLAGGED)
+        sq->state = UNCLICKED;
+    else if (sq->state == UNCLICKED)
+        sq->state = FLAGGED;
 }
